831925e9843b4845c507405592f13dfd8cfaeede
Kobe Vervoort <kobevervoort@gmail.com>
2017-04-26 13:38:08 +0200

    Initial commit: added index.php, nav.inc.php, nav.css, reset.css + nav icons

---
+++ nav.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<nav>
+
+    <a class="homeLink" href="">
+        <img src="img/homeInactive.png" alt="Home Link">
+        <p>tribune</p>
+    </a>
+    <a class="offerLink" href="">
+        <img src="img/offerInactive.png" alt="Tribune link">
+        <p>aanbiedingen</p>
+    </a>
+    <a class="profileLink" href="">
+        <img src="img/profileInactive.png" alt="Profiel link">
+        <p>profiel</p>
+    </a>
+    <a class="walletLink" href="">
+        <div>
+            <img src="img/coins.png" alt="wallet link">
+        </div>
+    </a>
+
+</nav>
