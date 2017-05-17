59a16b5ecf341e9b54e1aa600dfe5e131cc55797
Kobe Vervoort <kobevervoort@gmail.com>
2017-05-17 19:03:14 +0200

    Moved files around to fit folder structure

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+use \Bazaar\Classes\User;
+
+include_once 'Bazaar/start.php';
 
 $page = "tribune";
 
@@ -9,15 +12,15 @@ $page = "tribune";
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="Bazaar/css/reset.css">
     <link href="https://fonts.googleapis.com/css?family=Overpass:400,600" rel="stylesheet">
-    <link rel="stylesheet" href="css/nav.css">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="Bazaar/css/nav.css">
+    <link rel="stylesheet" href="Bazaar/css/index.css">
     <title>home</title>
 </head>
 <body>
-    <?php include_once 'header.inc.php';?>
-    <?php include_once 'nav.inc.php';?>
+    <?php include_once 'Bazaar/header.inc.php';?>
+    <?php include_once 'Bazaar/nav.inc.php';?>
     <section class="main">
 
     </section>
