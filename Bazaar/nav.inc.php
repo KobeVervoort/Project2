69ed32cd7591858c8587ba5ee3588b9073ef766b
Kobe Vervoort <kobevervoort@gmail.com>
2017-05-23 21:57:07 +0200

    Updated nav design

--- Bazaar/nav.inc.php
+++ Bazaar/nav.inc.php
@@ -1,44 +1,50 @@
 <?php
 
+
+
 ?>
 
 <nav>
 
 
-    <a class="homeLink" href="index.php">
+    <a class="homeLink" href="<?php echo BASE_URL . 'index.php'?>">
         <?php if($page == "tribune"):?>
-            <img src="img/homeActive.png" alt="Home Link">
+            <img src="<?php echo BASE_URL . 'Bazaar/img/homeActive.png'; ?> " alt="Home Link">
             <p class="red">tribune</p>
         <?php else: ?>
-            <img src="img/homeInactive.png" alt="Home Link">
+            <img src="<?php echo BASE_URL . 'Bazaar/img/homeInactive.png'; ?>" alt="Home Link">
             <p>tribune</p>
         <?php endif;?>
     </a>
 
-    <a class="offerLink" href="offers.php">
-        <?php if($page == "aanbiedingen"):?>
-            <img src="img/offerActive.png" alt="Tribune link">
+    <a class="offerLink" href="<?php echo BASE_URL . 'Bazaar/offers.php'?>">
+        <?php if($page == "offer"):?>
+            <img src="<?php echo BASE_URL . 'Bazaar/img/offerActive.png'; ?>" alt="Tribune link">
             <p class="red">aanbiedingen</p>
         <?php else: ?>
-            <img src="img/offerInactive.png" alt="Tribune link">
+            <img src="<?php echo BASE_URL . 'Bazaar/img/offerInactive.png'; ?>" alt="Tribune link">
             <p>aanbiedingen</p>
         <?php endif;?>
 
     </a>
-    <a class="profileLink" href="profile.php">
+    <a class="profileLink" href="<?php echo BASE_URL . 'Bazaar/profile.php'?>">
         <?php if($page == "profiel"):?>
-            <img src="img/profileActive.png" alt="Profiel link">
+            <img src="<?php echo BASE_URL . 'Bazaar/img/profileActive.png'; ?>" alt="Profiel link">
             <p class="red">profiel</p>
         <?php else: ?>
-            <img src="img/profileInactive.png" alt="Profiel link">
+            <img src="<?php echo BASE_URL . 'Bazaar/img/profileInactive.png'; ?>" alt="Profiel link">
             <p>profiel</p>
         <?php endif;?>
 
     </a>
-    <a class="walletLink" href="">
-        <div>
-            <img src="img/coins.png" alt="wallet link">
-        </div>
+    <a class="walletLink" href="<?php echo BASE_URL . 'Bazaar/wallet.php'?>">
+        <?php if($page == "wallet"):?>
+            <img src="<?php echo BASE_URL . 'Bazaar/img/walletActive.png'; ?>" alt="wallet link">
+            <p class="red">wallet</p>
+        <?php else: ?>
+            <img src="<?php echo BASE_URL . 'Bazaar/img/walletInactive.png'; ?>" alt="wallet link">
+            <p>wallet</p>
+        <?php endif;?>
     </a>
 
 </nav>
