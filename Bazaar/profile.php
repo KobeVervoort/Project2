eba11c8de022794fd07dbec680e18e9665d361da
Kobe Vervoort <kobevervoort@gmail.com>
2017-05-23 21:58:48 +0200

    Updated profile.php

--- Bazaar/profile.php
+++ Bazaar/profile.php
@@ -1,6 +1,8 @@
 <?php
 
-    $page = "profiel";
+include_once 'start.php';
+
+$page = "profiel";
 
 ?><!doctype html>
 <html lang="en">
@@ -10,12 +12,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/reset.css">
-    <link href="https://fonts.googleapis.com/css?family=Overpass:400,600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,600,800" rel="stylesheet">
     <link rel="stylesheet" href="css/nav.css">
     <title>profiel</title>
 </head>
 <body>
-    <?php include_once 'header.inc.php';?>
     <?php include_once 'nav.inc.php'?>
 </body>
 </html>
