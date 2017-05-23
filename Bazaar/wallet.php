df9e7c29a6612b9523a4c2be7d0a918fbe8fbdca
Kobe Vervoort <kobevervoort@gmail.com>
2017-05-23 21:55:15 +0200

    Added wallet.php and offer.php

---
+++ Bazaar/wallet.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once 'start.php';
+
+$page = 'wallet';
+
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/reset.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,600,800" rel="stylesheet">
+    <link rel="stylesheet" href="css/nav.css">
+    <title>wallet</title>
+</head>
+<body>
+    <?php include_once 'nav.inc.php'?>
+</body>
+</html>
