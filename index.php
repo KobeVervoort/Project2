f94ef9cf0edd829d4591c8c6832c33b0319fc6ff
Kobe Vervoort <kobevervoort@gmail.com>
2017-06-10 15:02:20 +0200

    Added favicons

--- index.php
+++ index.php
@@ -24,6 +24,12 @@ if(isset($_SESSION['userID'])){
 ?><!doctype html>
 <html lang="en">
 <head>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -32,6 +38,7 @@ if(isset($_SESSION['userID'])){
     <link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,600,800" rel="stylesheet">
     <link rel="stylesheet" href="Bazaar/css/nav.css">
     <link rel="stylesheet" href="Bazaar/css/index.css">
+
     <title>home</title>
 </head>
 <body>
@@ -76,9 +83,6 @@ if(isset($_SESSION['userID'])){
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script>
-        document.documentElement.requestFullscreen();
-    </script>
 
 </body>
 
